03f9c1b03f6b8da56c157ebc3c268575d050afe2
Gabriel Marcos <gabrielmareia@hotmail.com>
2017-05-18 21:02:48 -0300

    Create news methods

--- vue-teste.js
+++ vue-teste.js
@@ -1,30 +1,41 @@
+var defaults = ['Date', 'Campaing']
+var tags = ['utm_source=whirlpool_emkt_home_', '_principal&utm_medium=emkt&utm_campaign=']
+
 var app = new Vue({
 	el:'#app',
   data: {
   	link: 'http://loja.brastemp.com.br/',
-    date:'',
+    params: [],
     UTMIcheck: true,
-    UTM:{
-      source: '',
-      medium: '',
-      campaing: '',
-      UTMI: ''
-    },
-    currently: '',
-    
     results:[]
+    
   },
+  created: function(){
+    this.setup()
+  },
+
   methods:{
-		buildUtm: function() {
-      this.result = 
-      this.link + "?utm_source=" +
-      this.UTM.source + this.date + "_principal&utm_medium=" + 
-      this.UTM.medium + "&utm_campaign=" + this.UTM.campaing + 
-      this.UTM.UTMI  
-      this.results.push(this.result)
+    setup: function(){
+      defaults.forEach(function(element) {
+        this.params.push({key:element, value:''})
+      }, this);
     },
+
     remove: function(index) {
       this.results.splice(index, 1);
+    },
+
+    save: function(){
+      this.results.push(this.result)
+    }
+  },
+  computed:{
+    result: function(){
+
+      return this.link +this.params.map(function(tags){
+        return tags.key + "=" + tags.value
+      }).join("&")
+      
     }
   }
 
